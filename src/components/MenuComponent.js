50821b978c2b8a689012691638864826160246c8
Karanbir Singh <krnbr@live.in>
2018-08-03 00:27:53 +0530

    changes

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,43 +1,21 @@
 import React, {Component} from 'react';
 import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
-import Dishdetail from './DishdetailComponent';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            selectedDish: null
-        };
         console.log("Menu component constructor called");
-    }
-
-    onDishSelect(dish){
-        this.setState({
-            selectedDish: dish
-        });
     }
 
     componentDidMount(){
         console.log("Menu component did mount");
     }
 
-    renderDish(dish){
-        if (dish != null){
-            return (
-                <Dishdetail dish={dish}/>
-            );
-        } else {
-            return (
-                <div></div>
-            );
-        }
-    }
-
     render() {
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
-                    <Card onClick={() => this.onDishSelect(dish)}>
+                    <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
                         <CardImg width="100%" object src={dish.image} alt={dish.name}/>
                         <CardImgOverlay body className="ml-5">
                             <CardTitle>{dish.name}</CardTitle>
@@ -54,7 +32,6 @@ class Menu extends Component {
                 <div className="row">
                     {menu}
                 </div>
-                {this.renderDish(this.state.selectedDish)}
             </div>
         );
     }
