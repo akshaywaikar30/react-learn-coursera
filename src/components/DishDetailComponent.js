596e5b03bf1380d67ec204aac94b9f65df71c254
Karanbir Singh <krnbr@live.in>
2018-08-05 07:56:55 +0530

    changes for lifecyles part 2

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,36 +3,50 @@ import {Card, CardImg, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class Dishdetail extends Component {
 
-    constructor(props) {
-        super(props);
+    componentDidMount(){
+        console.log("Dishdetail component did mount");
+    }
 
-        console.log("Dishdetail component constructor called");
+    componentDidUpdate(){
+        console.log("Dishdetail component did update");
     }
 
     renderDish(dish){
-        return (
-            <div className="col-12 col-md-5 m-1">
-                <Card>
-                    <CardImg top src={dish.image} alt={dish.name} />
-                    <CardBody>
-                        <CardTitle>{dish.name}</CardTitle>
-                        <CardText>{dish.description}</CardText>
-                    </CardBody>
-                </Card>
-            </div>
-        );
+        console.log("Dishdetail component rendered");
+        if(dish!=null){
+            return (
+                <div className="col-12 col-md-5 m-1">
+                    <Card>
+                        <CardImg top src={dish.image} alt={dish.name} />
+                        <CardBody>
+                            <CardTitle>{dish.name}</CardTitle>
+                            <CardText>{dish.description}</CardText>
+                        </CardBody>
+                    </Card>
+                </div>
+            );
+        }else {
+            return (
+                <div className="col-12 col-md-5 m-1"></div>
+            );
+        }
     }
 
-    renderComments(comments) {
-        if (comments != null) {
+    renderComments(dish) {
+        if (dish != null) {
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
-                        {comments.map((comment) => (
+                        {dish.comments.map((comment) => (
                             <li key={comment.id}>
                                 <p>{comment.comment}</p>
-                                <p>-- {comment.author}, {comment.date}</p>
+                                <p>-- {comment.author}, {
+                                    new Intl.DateTimeFormat('en-US', {
+                                        year: 'numeric', month: 'short', day: '2-digit'
+                                    }).format(new Date(Date.parse(comment.date)))
+                                    }
+                                </p>
                             </li>
                         ))}
                     </ul>
@@ -47,9 +61,11 @@ class Dishdetail extends Component {
 
     render() {
         return (
-            <div className="row">
-                {this.renderDish(this.props.dish)}
-                {this.renderComments(this.props.dish.comments)}
+            <div className="container">
+                <div className="row">
+                    {this.renderDish(this.props.dish)}
+                    {this.renderComments(this.props.dish)}
+                </div>
             </div>
         );
     }
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
 class Menu extends Component {
+
+
+
     constructor(props) {
         super(props);
         console.log("Menu component constructor called");
@@ -16,8 +19,8 @@ class Menu extends Component {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
                     <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}>
-                        <CardImg width="100%" object src={dish.image} alt={dish.name}/>
-                        <CardImgOverlay body className="ml-5">
+                        <CardImg width="100%" src={dish.image} alt={dish.name}/>
+                        <CardImgOverlay className="ml-5">
                             <CardTitle>{dish.name}</CardTitle>
                         </CardImgOverlay>
                     </Card>
